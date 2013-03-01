112320fe0d1f2f29f46fdcb03176a37f76cde4b7
Florian Fankhauser <fankiflo@gmail.com>
2013-03-01 15:49:17 +0100

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,28 +1,79 @@
+var selectedMonth;
+var selectedYear;
 
-function getStartDate() {
-  start = new Date();
-  start.setDate(1);
-  start.setMilliseconds(start.getMilliseconds() - 86400000 * (6 + start.getDay()));
+function getStartDate(month, year) {
+  start = new Date(year, month, 1);
+  start.setTime(start.getTime() - 86400000 * (6 + start.getDay()));
   return start;
+}
+function getEndDate(month, year) {
+  start = getStartDate(month, year);
+	end = new Date(start.getTime() + 48 * 86400000);
+	return end;
 }
 
 function initCalendar() {
-  date = getStartDate();
+  selectedMonth = (new Date()).getMonth();
+  selectedYear = (new Date()).getFullYear();
+  date = getStartDate(selectedMonth, selectedYear);
   var calendar = $(".calendar tbody");
   html = "";
   for (var w = 0; w < 7; w++) {
-    html += "<tr class=\"week\">";
-    for (var d = 0; d < 7; d++) {
-      html += "<td class=\"day";
-      if (date.getMonth() == 1) {
-        html += " bold";
-      }
-      html += "\" id=\"" + date.getYear() + "." + (date.getMonth() + 1) + "."+ date.getDate() + "\">" + date.getDate() + "</td>";
-      date.setMilliseconds(date.getMilliseconds() + 86400000);
-    }
-    html += "</tr>";
+    html += getWeekHtml(new Date(date.getTime()));
   }
   calendar.html(html);
+  $(".week").show();
+  console.log("." + selectedYear + "." + selectedMonth);
+  $(".y" + selectedYear + ".m" + selectedMonth).addClass("bold");
+}
+
+function getWeekHtml(startdate) {
+  console.log("getWeekHtml called with: " + startdate.toGMTString());
+  var weekHtml = "";
+  if (startdate.getDay() != 1) {
+    console.log("Unvalid Week start: " + startdate.toGMTString());
+  } else {
+    date = startdate;
+    weekHtml += "<tr class=\"week\" style=\"display: none\">";
+    for (var d = 0; d < 7; d++) {
+      weekHtml += "<td class=\"day";
+      weekHtml += " y" + date.getFullYear() + " m" + date.getMonth() + " d"+ date.getDate() + "\">" + date.getDate() + "</td>";
+      date.setTime(date.getTime() + 86400000);
+    }
+    weekHtml += "</tr>";
+    return weekHtml;
+  }
+}
+
+function nextMonth() {
+  console.log("nexMonth called");
+  $(".y" + selectedYear + ".m" + selectedMonth).removeClass("bold");
+  previousEnd = getEndDate(selectedMonth, selectedYear);
+  selectedMonth = (selectedMonth + 1) % 12;
+  if (selectedMonth == 0) {
+    selectedYear += 1;
+  }
+  end = getEndDate(selectedMonth, selectedYear);
+  weekstart = new Date(previousEnd.getTime() + 86400000);
+  console.log(weekstart);
+  console.log(end);
+  var switchcount = 0;
+  while (weekstart < end) {
+    $(".calendar tbody").append(getWeekHtml(weekstart));
+    switchcount++;
+  } 
+  slideWeeksUp(switchcount);
+  $(".y" + selectedYear + ".m" + selectedMonth).addClass("bold");
+}
+
+function slideWeeksUp(num) {
+  $(".week:first").fadeOut(100, function() {
+    this.remove();
+    $(".week").slice(6,7).fadeIn(100);
+    if (num > 1) {
+      slideWeeksUp(--num);      
+    }
+  });
 }
 
 function debug(string) {
@@ -38,6 +89,10 @@ $(document).ready(function() {
     } else {
       $(this).addClass("selected");
     }
+  });
+  
+  $(".forth").click(function() {
+    nextMonth();
   });
 });
 
