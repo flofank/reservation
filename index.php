860322723874b7b88e3c7713635f5dc309ea36b9
Florian Fankhauser <fankiflo@gmail.com>
2013-03-18 19:35:10 +0100

    widget adde widget addedd

--- index.php
+++ index.php
@@ -10,57 +10,70 @@
 </head>
 <body>
     <div class="page_head">
-        <span>BOOK ME</span>
-    </div>
-    
-    <div class="widget menu">
-        <div class="menu_item top">
-            <span>Menu</span>
-        </div>
-        <div class="menu_item">
-            <span>Menu-Item 1</span>
-        </div>
-        <div class="menu_item">
-            <span>Some other Menu-Item</span>
-        </div>
-        <div class="menu_item bottom">
-            <span>Some other Menu-Item</span>
+        <div>BOOK ME</div>
+        <div>
+            <input class="search" type="search" name="localsearch"/>
         </div>
     </div>
     
-    <div class="widget content">
-        <div class="calendar-wrapper" style="display: none">
-		<div class="month tripple_float">
-			<div class="side">
-				<div class="switch back"></div>
-			</div>
-			<div class="mid">
-				<div class="name">FEBRUARY</div>
-			</div>
-			<div class="side">
-				<div class="switch forth"></div>
-			</div>
-			<div class="nofloat"></div>
-		</div>
-		<div class="nofloat"></div>
-		<table class="calendar" cellspacing="0">
-			<thead>
-				<tr>
-					<th>MO</th>
-					<th>TU</th>
-					<th>WE</th>
-					<th>TH</th>
-					<th>FR</th>
-					<th>SA</th>
-					<th>SU</th>
-				</tr>
-			</thead>
-			<tbody>
-			</tbody>
-		</table>
-	</div>        
+    <div class="page_content content_left">
+        <div class="widget menu">
+            <div class="menu_item top">
+                <span>Menu</span>
+            </div>
+            <div class="menu_item">
+                <span>Menu-Item 1</span>
+            </div>
+            <div class="menu_item">
+                <span>Some other Menu-Item</span>
+            </div>
+            <div class="menu_item bottom">
+                <span>Some other Menu-Item</span>
+            </div>
+        </div>
+        
+        <div class="widget">
+            Just some other widget
+        </div>
     </div>
     
+    <div class="page_content content_right">
+        <div class="widget content">
+            <div class="calendar-wrapper" style="display: none">
+                    <div class="month tripple_float">
+                            <div class="side">
+                                    <div class="switch back"></div>
+                            </div>
+                            <div class="mid">
+                                    <div class="name">FEBRUARY</div>
+                            </div>
+                            <div class="side">
+                                    <div class="switch forth"></div>
+                            </div>
+                            <div class="nofloat"></div>
+                    </div>
+                    <div class="nofloat"></div>
+                    <table class="calendar" cellspacing="0">
+                            <thead>
+                                    <tr>
+                                            <th>MO</th>
+                                            <th>TU</th>
+                                            <th>WE</th>
+                                            <th>TH</th>
+                                            <th>FR</th>
+                                            <th>SA</th>
+                                            <th>SU</th>
+                                    </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                    </table>
+            </div>        
+        </div>
+    </div>
+    
+    
+    <div class="nofloat"></div>
     <div id="debug"></div>
 </body>
 </html>
