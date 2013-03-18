704f4fffc58d2acfea259780e1b0b93cf5c6a976
Florian Fankhauser <flofank@gmx.ch>
2013-03-18 18:15:01 +0100

    # Please enter the commit message for your changes. Lines starting

--- index.php
+++ index.php
@@ -13,10 +13,23 @@
         <span>BOOK ME</span>
     </div>
     
-    <div class="widget menu">Menu</div>
+    <div class="widget menu">
+        <div class="menu_item top">
+            <span>Menu</span>
+        </div>
+        <div class="menu_item">
+            <span>Menu-Item 1</span>
+        </div>
+        <div class="menu_item">
+            <span>Some other Menu-Item</span>
+        </div>
+        <div class="menu_item bottom">
+            <span>Some other Menu-Item</span>
+        </div>
+    </div>
     
     <div class="widget content">
-        <div class="calendar-wrapper">
+        <div class="calendar-wrapper" style="display: none">
 		<div class="month tripple_float">
 			<div class="side">
 				<div class="switch back"></div>
