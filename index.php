dcadfb507947b113800a20be18ee31265287c0c6
Florian Fankhauser <flofank@gmx.ch>
2013-05-21 22:06:30 +0200

    added create.html and removed some old version files

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-    <script src="js/jquery.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <script type="text/javascript" src="js/jquery.js"></script>
     <link rel="stylesheet" type="text/css" href="css/styles.css" />
     <link rel="stylesheet" type="text/css" href="css/calendar.css" />
     <script type="text/javascript" src="js/main.js"></script>
