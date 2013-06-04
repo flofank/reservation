7cb6b5fc586db1ab795c329b3914dd7afccb5b6a
Florian Fankhauser <flofank@gmx.ch>
2013-06-04 08:45:12 +0200

    added some information messages to welcome.php

--- welcome.php
+++ welcome.php
@@ -4,4 +4,12 @@
     <a href='index.php?admin'>Neues Reservationsobjekt erstellen</a>
     <br><br>
     <a href='index.php?show=reservHash'>Demo-Objekt</a>
+    <div><br/>
+        Hinweise
+        <ul>
+            <li>Falls bei der Anzeige der Bilder Probleme autauchen bitte die Seite neuladen.</li>
+            <li>Diese Seite ist ein nicht kompletter Prototyp. Das heisst, das gewisse Funktionen noch nicht voll funktionsfähig sind.</li>
+            <li>Dieser Prototyp wurde für Google Chrome entwickelt. Browserkompatibilität wurde beim Prototyping ignoriert, weshalb in anderen Browsern fehlerhafte Darstellungen vorkommen können.
+        </ul>
+    </div>
 </div>
